bfb0cfbd94ff859068258443e7a9603cfae569cd
nishu kumar <kumarnishu437@gmail.com>
2023-06-22 10:22:01 +0530

    updated

--- app/src/main/java/com/nishu/androidprojects/MainActivity.kt
+++ app/src/main/java/com/nishu/androidprojects/MainActivity.kt
@@ -6,24 +6,64 @@ import androidx.activity.compose.setContent
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.ui.res.painterResource
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard("Nishu kumar")
+            MessageCard(Message("Android", "Jetpack Compose"))
         }
     }
 }
 
-@Composable
-fun MessageCard(name:String){
-    Text(text="Hello $name")
-}
+data class Message(val author: String, val body: String)
 
+
+@Composable
+fun MessageCard(msg: Message) {
+    // Add padding around our message
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.ic_launcher_background),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                // Set image size to 40 dp
+                .size(40.dp)
+                // Clip image to be shaped as a circle
+                .clip(CircleShape)
+        )
+
+        // Add a horizontal space between the image and the column
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            Text(text = msg.author)
+            // Add a vertical space between the author and message texts
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = msg.body)
+        }
+    }
+}
 
 @Preview
 @Composable
 fun PreviewMessageCard() {
-    MessageCard("Nishu kumarr")
+    MessageCard(
+        msg = Message("Author", "Nishu kumar")
+    )
 }
+
